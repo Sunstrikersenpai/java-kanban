4eb842beeeba975d5e58ecd7a1af07ba9703c2c3
Sunstriker <sunstrikersenpai@gmail.com>
2025-03-16 08:50:15 +0300

    sprint 9 solution

---
+++ src/httpServer/adapters/LocalDateTimeAdapter.java
@@ -0,0 +1,28 @@
+package httpserver.adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    @Override
+    public void write(final JsonWriter jsonWriter, final LocalDateTime localDate) throws IOException {
+        if (localDate == null) {
+            jsonWriter.nullValue();
+            return;
+        }
+        jsonWriter.value(localDate.format(dtf));
+    }
+
+    @Override
+    public LocalDateTime read(final JsonReader jsonReader) throws IOException {
+        return LocalDateTime.parse(jsonReader.nextString(), dtf);
+    }
+}
+
